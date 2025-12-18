648086dd40059ab6607e0f1afe439dac49180e18
pawarsrujal <srujalpawar2004@gmail.com>
2025-12-18 20:22:30 +0530

    requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="srujal pawar",
+    author_email="srujalpawar2004@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
