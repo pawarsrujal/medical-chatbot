99562cc9b44ca4faebb5e040cfa4629f917a5896
pawarsrujal <166549577+pawarsrujal@users.noreply.github.com>
2025-12-23 18:28:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -190,4 +190,4 @@ def clear_chat():
 # ---------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=10000, debug=False)
